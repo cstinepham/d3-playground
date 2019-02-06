58546dbada418cff252d38d57928e2c504eab50f
christine.pham <cstinepham@gmail.com>
2019-02-06 10:17:11 -0800

    Implement rects for different building sizes

---
+++ 2/2.08/js/main.js
@@ -0,0 +1,27 @@
+
+d3.json("./data/buildings.json").then((data) => {
+  data.forEach((d) => {
+    d.height = +d.height;
+  });
+  
+  var svg = d3.select("#chart-area").append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
+
+  
+  var rects = svg.selectAll("rect")
+    .data(data);
+
+  rects.enter()
+    .append("rect")
+      .attr("x", (d, i) => {
+        return (i * 50) + 25;
+      })
+      .attr("width", 30)
+      .attr("height", (d) => {
+        return d.height;
+      })
+      .attr("fill", "gray");
+  
+})
+
