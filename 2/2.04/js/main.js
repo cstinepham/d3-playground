2a874b472a26419b0dfec2d7a25148ed2dcb2ccf
christine.pham <cstinepham@gmail.com>
2019-02-06 09:48:10 -0800

    Translate data array in to circle sizes

--- 2/2.04/js/main.js
+++ 2/2.04/js/main.js
@@ -7,4 +7,11 @@ var circle = svg.append("circle")
   .attr("cx", 200)
   .attr("cy", 200)
   .attr("r", 100)
-  .attr("fill", blue);
+  .attr("fill", "blue");
+
+var rectangle = svg.append("rect")
+  .attr("x", 50)
+  .attr("y", 20)
+  .attr("width", 50)
+  .attr("height", 200)
+  .attr("fill", "pink");
---
+++ 2/2.06/js/main.js
@@ -0,0 +1,30 @@
+var data = [25,20,10,12,15];
+
+var svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+// var circle = svg.append("circle")
+//   .attr("cx", 200)
+//   .attr("cy", 200)
+//   .attr("r", 100)
+//   .attr("fill", "red");
+
+ var circles = svg.selectAll("circle")
+  .data(data);
+  
+  circles.enter()
+    .append("circle")
+      .attr("cx", (d,i) => {
+        console.log("Item: " + d, "Index: " + i);
+        return (i * 50) + 25;
+      })
+      .attr("cy", 25)
+      .attr("r", (d) => {
+        console.log("Item: " + d);
+        return d;
+      })
+      .attr("fill", "red");
+
+  
+
